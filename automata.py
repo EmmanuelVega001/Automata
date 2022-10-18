8ba770a4c61ed4ec22c4c9f2a35abf86dc52bc9d
EmmanuelVega001 <72900109+EmmanuelVega001@users.noreply.github.com>
2022-10-18 05:49:01 -0500

    planteamineto secundario

--- automata.py
+++ automata.py
@@ -1,9 +1,6 @@
-f = open("texto.txt")
-for line in f:
-    for caracter in line :
-        linea=f.read(1)
-        print(linea)
-        arregloLinea=[]
-        linea.split()
-        arregloLinea.append(linea)
-        print(arregloLinea)
+f = open("texto.txt", "r")
+ver=0
+nueva=f.read()
+nueva.replace("\n", "")
+x=nueva.count("hola")
+print(x)
