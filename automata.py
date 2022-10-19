a588ce1e71584bdfa6e53f658de5a31bdf9af4b4
EmmanuelVega001 <72900109+EmmanuelVega001@users.noreply.github.com>
2022-10-18 23:55:03 -0500

    Validacion de cadena

--- automata.py
+++ automata.py
@@ -1,20 +1,19 @@
 f = open("texto.txt")
+#Creación de arreglos necesarios para hacer la validacion
 arregloLinea=[]
 cadenaCheck=["h","o","l","a"]
 cadenaTemporal=[]
-ver=0
-for line in f:
-    for caracter in line :
-        cont=0
-        arregloLinea.append(caracter)
-        caractertemp=caracter
-        cadenaTemporal.append(caractertemp)
-        
-        if arregloLinea[cont] == "h":
-            if arregloLinea[cont] == "o":
-                if arregloLinea[cont] == "l":
-                    if arregloLinea[cont] == "a":
-                        ver+=1
-    cont+=1           
+ver=0 #verificacion
+for line in f:#Por cada linea del documento
+    depuradoLine=line.replace("\n","") #Eliminar los saltos de linea del documento
+    for caracter in depuradoLine :
+        cont=0 #Contador para la deteccion de numeros
+        arregloLinea.append(caracter)#Agregamos al arreglo de la linea todo lo que se encuentra en la linea
+        cadenaTemporal.append(caracter)#Arreglo que va a cambiar de estado para verificar si se encuentra la palaba
+        if cadenaTemporal[cont] == cadenaCheck[cont]:#Verificamos si el caracter que se busca esta en el arreglo para aceptar
+            ver+=1#Aumentamos la verificacion 
+    cont+=1#
+    cadenaTemporal.clear()
+ver/4      
 print(cadenaTemporal)
-print (ver)
+print (int(ver/4))
